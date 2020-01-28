323414e01e381faf4eaccbdfa6dcb1f435bed542
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2020-01-28 09:44:39 -1000

    Initial Commit

---
+++ Hackwich2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hackwich2
+//
+//  Created by Keola Iseri on 1/28/20.
+//  Copyright © 2020 Keola Iseri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
