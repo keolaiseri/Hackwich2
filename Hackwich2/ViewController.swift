d4b6a4dd86a09cf22406e3dc8adfb4c7adeceda5
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2020-01-28 10:40:12 -1000

    efsdsdfsdfs

---
+++ Hackwich2/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Hackwich2
+//
+//  Created by Keola Iseri on 1/28/20.
+//  Copyright © 2020 Keola Iseri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func pressMePressedButton(_ sender: Any)
+    {
+      print("Hello World")
+        
+    }
+    
+    @IBAction func pressMeButtonAbove(_ sender: Any)
+    {
+      print("Goodbye World")
+    
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
